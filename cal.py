f2aa6a616a7cbe39b111e475cc0b1c10608787f0
sakshijagdale18 <145276621+sakshijagdale18@users.noreply.github.com>
2023-10-08 09:22:01 +0530

    Add files via upload

---
+++ cal.py
@@ -0,0 +1,14 @@
+choice=input('Choose operator +,-,*,/')
+num1=int(input("Enter first number"))
+num2=int(input("Enter second number"))
+if choice=="+":
+    print(num1+num2)
+elif choice=="-":
+    print(num1+num2)
+elif choice=="*":
+    print(num1+num2)
+elif choice=="/":
+    print(num1+num2)
+else:
+    print("Invalid choice")
+
