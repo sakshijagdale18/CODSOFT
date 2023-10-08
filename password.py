7ffe02e4051acbf4a20f28cc977e850d42ac6bf8
sakshijagdale18 <145276621+sakshijagdale18@users.noreply.github.com>
2023-10-08 09:12:04 +0530

    Add files via upload

---
+++ password.py
@@ -0,0 +1,15 @@
+import random
+import string
+print("Password Generator")
+def main():
+    length=int(input("Enter the length of password"))
+    lowerD=string.ascii_lowercase
+    upperD=string.ascii_uppercase
+    digitD=string.digits
+    symbolsD=string.punctuation
+    combine=lowerD+upperD+digitD+symbolsD
+    x=random.sample(combine,length)
+    password="".join(x)
+    print(password)
+    main()
+main()
